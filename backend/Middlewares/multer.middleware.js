95c2c4fe42b36ae401a0cf8801ef092ac71c83ee
ZaidKha2321 <zaidkhan232125@gmail.com>
2024-09-02 17:11:28 +0530

    Do not delete duplicate files, each corresponds to unique product even though they use same images.

--- backend/Middlewares/multer.middleware.js
+++ backend/Middlewares/multer.middleware.js
@@ -1,10 +1,10 @@
 import multer from "multer";
-import fs, { readdir } from 'fs' ;
-import path from "path";
-import { promisify } from "util";
+// import fs, { readdir } from 'fs' ;
+// import path from "path";
+// import { promisify } from "util";
 
-const readdirec = promisify(fs.readdir) ;
-const unlink = promisify(fs.unlink) ;
+// const readdirec = promisify(fs.readdir) ;
+// const unlink = promisify(fs.unlink) ;
 
 const storageConfig = multer.diskStorage({
     destination: (req, file, cb)=>{
@@ -13,18 +13,18 @@ const storageConfig = multer.diskStorage({
     filename: async (req, file, cb)=>{
         const uniqueName = Date.now() + "-" + file.originalname ;
         // Check duplicates
-        try{
-            const files = await readdirec('./assets/Product.Images/') ;
-            for(const existingFile of files){
-                const [timeStampPart, existingOriginaName] = existingFile.split('-', 2) ;
-                if(existingOriginaName === file.originalname){
-                    const existingFilePath = path.join('./assets/Product.Images/', existingFile) ;
-                    await unlink(existingFilePath) ;
-                }
-            }
-        }catch(err){
-            console.log(err) ;
-        }
+        // try{
+        //     const files = await readdirec('./assets/Product.Images/') ;
+        //     for(const existingFile of files){
+        //         const [timeStampPart, existingOriginaName] = existingFile.split('-', 2) ;
+        //         if(existingOriginaName === file.originalname){
+        //             const existingFilePath = path.join('./assets/Product.Images/', existingFile) ;
+        //             await unlink(existingFilePath) ;
+        //         }
+        //     }
+        // }catch(err){
+        //     console.log(err) ;
+        // }
         // Add new file
         cb(null, uniqueName) ;
     }
